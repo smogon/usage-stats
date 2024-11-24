cacfc95f4892386b1db82d094a82484f022c1de2
larry-the-table-guy <180724454+larry-the-table-guy@users.noreply.github.com>
2024-11-24 11:26:08 -0500

    Simplify and optimize keyify function

--- common.py
+++ common.py
@@ -5,15 +5,11 @@ import math
 import js2py
 import urllib2
 import ujson as json
+import re
 
+nonKeyRegex = re.compile('[^a-z0-9]+')
 def keyify(s):
-	sout = ''
-	for c in s:
-		if c in string.uppercase:
-			sout = sout + c.lower()
-		elif c in string.lowercase + '1234567890':
-			sout = sout + c
-	return sout
+	return nonKeyRegex.sub('', s.lower())
 
 #our weighting function
 def weighting(rating,deviation,cutoff):
